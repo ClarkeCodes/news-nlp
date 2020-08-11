789390969d8f5c0332afda4b018a155f2db763d8
ElineJ <elinejacobse@gmail.com>
2020-08-11 11:49:30 -0400

    Chore: updated prod configs

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,16 @@ import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
-alert("I EXIST")
 console.log("CHANGE!!");
+
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/footer.scss';
+
+
+export {
+    checkForName,
+    handleSubmit
+}
