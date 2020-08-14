082011d0663bf38489c79077bb45ed83330c6209
ElineJ <elinejacobse@gmail.com>
2020-08-14 11:46:47 -0400

    chore: setup dotenv config with API key

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,6 @@ import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
-console.log("CHANGE!!");
-
 import './styles/resets.scss';
 import './styles/base.scss';
 import './styles/form.scss';
--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,24 @@
 var path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 const app = express();
 
-app.use(express.static('dist'));
+// console.log(__dirname)
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-console.log(__dirname)
+app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -15,9 +27,12 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
+    console.log(`Your API key is ${process.env.API_KEY}`);
     console.log('Example app listening on port 8080!');
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse);
+    console.log(`Your API key is ${process.env.API_KEY}`);
+    console.log(mockAPIResponse);
+    res.send(process.env.API_KEY);
 })
